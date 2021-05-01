16c6890f5310c0a6a7673e657c4b6e0c0a78d071
Blake Weber <trapz@protonmail.ch>
2021-05-01 12:27:25 -0400

    winsales start form

---
+++ app/Models/Winsales/PaperOrder.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Winsales;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PaperOrder extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2021_05_01_162344_create_paper_orders_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaperOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paper_orders', function (Blueprint $table) {
+            $table->id();
+/*
+Vendor Name:		Vendor ID/Number:	
+Sales Agent ID number			
+Selected Bundle:	Solo or BB/Phone Bundle?	Sale Type:	Consumer/SMB
+Selected_HSI_Speed:		Current Windstream number: (for broadband adds and upgrades)	
+Selected_bundle_adder: Secure/Security/Youtube		Expected Install Date:	
+Customer Name:		Modem Rental:	
+Customer Email:		Complete below if billing address differs from service address:	
+Customer Contact Number:		Billing Address:	
+Text Msg Opt in:		Billing City:	
+Customer  Service Address:		Billing State:	
+County:		Billing Zip Code:	
+City:		Phone Book Name (If listing is requested):	
+State:		Complete below if customer is porting a number:	
+Zip Code:		Current Provider:	Port In Phone No 1:
+Authorized Users:		Current Acct No:	Port In Phone No 2:
+Additional Contact Number:		Current PIN:	
+Referral ID:			
+Lifeline QCODE:			
+Applying for EBB:			
+*/
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paper_orders');
+    }
+}
