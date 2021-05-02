6c7d0786cbaebb973bc7c0c677b8f4e12153662c
Blake Weber <trapz@protonmail.ch>
2021-05-02 11:30:32 -0400

    cols

--- database/migrations/2021_05_01_162344_create_paper_orders_table.php
+++ database/migrations/2021_05_01_162344_create_paper_orders_table.php
@@ -16,10 +16,27 @@ class CreatePaperOrdersTable extends Migration
         Schema::create('paper_orders', function (Blueprint $table) {
             $table->id();
             $table->string('vendor_name');
-            $table->string('agent_id');
-            $table->string('vendor_name');
-            $table->string('vendor_name');
-            
+            $table->string('vendor_ex_id');
+            $table->string('agent_empid');
+            $table->string('sale_type');
+            $table->string('bundle_type');
+            $table->string('speed')->nullable();
+            $table->json('bundle_adders')->nullable();
+            $table->string('cust_name');
+            $table->string('cust_email');
+            $table->string('cust_cbr');
+            $table->string('cust_opt_in');
+            $table->string('service_address_line_1');
+            $table->string('service_address_line_2');
+            $table->string('service_address_county');
+            $table->string('service_address_city');
+            $table->string('service_address_zip');
+            $table->string('install_date');
+            $table->string('cust_existing_tn');
+            $table->string('');
+            $table->string('');
+            $table->string('');
+            $table->string('');
             
 /*
 Vendor Name:		Vendor ID/Number:	
